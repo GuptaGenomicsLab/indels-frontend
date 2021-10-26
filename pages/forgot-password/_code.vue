856f6a6724840e462b0b35f8838f839e1547eb81
David Kanter Eivin <kanter.dev@gmail.com>
2021-10-26 15:21:21 -0400

    feat: add password reset functionality

---
+++ pages/forgot-password/_code.vue
@@ -0,0 +1,108 @@
+<template>
+  <div id='login-box' class='content-box'>
+    <form @submit.prevent='submit'>
+      <header>
+        <DnaIcon size='lg' animated />
+        <h2 class='black'>Password Reset Form</h2>
+        <p>
+          Please enter your new password.
+        </p>
+      </header>
+      <b-field>
+        <b-input v-model='formData.password'
+                 placeholder='Password'
+                 type='password'
+                 icon='lock'
+                 password-reveal
+        />
+      </b-field>
+      <b-field>
+        <b-input v-model='formData.passwordConfirm'
+                 placeholder='Password Confirm'
+                 type='password'
+                 icon='lock'
+                 password-reveal
+        />
+      </b-field>
+      <button id='login-button' class='primary-button space-hz-sm' type='submit'>Request Reset</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { reactive, useRoute, useRouter } from '@nuxtjs/composition-api'
+import DnaIcon from '@/components/DnaIcon'
+import { useAxios } from '@/scripts/useHooks'
+import { rejectionToast, successToast } from '@/scripts/ui'
+
+export default {
+  name: 'ResetPasswordPage',
+  components: { DnaIcon },
+  setup() {
+    const axios = useAxios()
+    const route = useRoute()
+    const router = useRouter()
+
+    const formData = reactive({
+      password: '',
+      passwordConfirm: ''
+    })
+
+    const submit = async () => {
+      if (formData.password !== formData.passwordConfirm)
+        return rejectionToast('Password and confirm password fields must match.')
+
+      const res = await axios.$post(`/user/reset/${route.value.params.code}`, {
+        password: formData.password
+      })
+      if (res) {
+        successToast('Password Reset. Please log in.')
+        router.push({ path: '/' })
+      }
+    }
+
+
+    return {
+      submit,
+      formData
+    }
+  }
+}
+</script>
+
+<style scoped lang='scss'>
+@import '~assets/scss/globals.scss';
+
+form {
+  header {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  footer {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+
+  @include respond-below(sm) {
+    width: 100%;
+  }
+  @include respond-between(sm, lg) {
+    width: 90%;
+  }
+  @include respond-between(lg, xlg) {
+    width: 80%;
+  }
+  @include respond-above(xlg) {
+    width: 60%;
+  }
+
+  display: flex;
+  margin: 10% auto;
+  flex-direction: column;
+  padding: 10px;
+}
+
+</style>
---
+++ pages/forgot-password/index.vue
@@ -0,0 +1,87 @@
+<template>
+  <div id='login-box' class='content-box'>
+    <form @submit.prevent='submit'>
+      <header>
+        <DnaIcon size='lg' animated />
+        <h2 class='black'>Request a Password Reset</h2>
+        <p>
+          Please enter the email associated with your account.
+          We will email you a link to reset your password.
+        </p>
+      </header>
+      <b-field>
+        <b-input v-model='formData.email'
+                 placeholder='Email'
+                 icon='at'
+        />
+      </b-field>
+      <button id='login-button' class='primary-button space-hz-sm' type='submit'>Request Reset</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { reactive } from '@nuxtjs/composition-api'
+import DnaIcon from '@/components/DnaIcon'
+import { useAxios } from '@/scripts/useHooks'
+import { successToast } from '@/scripts/ui'
+
+export default {
+  name: 'ForgotPasswordPage',
+  components: { DnaIcon },
+  setup() {
+    const axios = useAxios()
+
+    const formData = reactive({
+      email: ''
+    })
+
+    const submit = async () => {
+      const res = await axios.$post(`/user/request-reset/${formData.email}`);
+      if (res) successToast('Reset Requested. Please close this page and check your email.')
+    }
+
+    return {
+      submit,
+      formData
+    }
+  }
+}
+</script>
+
+<style scoped lang='scss'>
+@import '~assets/scss/globals.scss';
+
+form {
+  header {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  footer {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+
+  @include respond-below(sm) {
+    width: 100%;
+  }
+  @include respond-between(sm, lg) {
+    width: 90%;
+  }
+  @include respond-between(lg, xlg) {
+    width: 80%;
+  }
+  @include respond-above(xlg) {
+    width: 60%;
+  }
+
+  display: flex;
+  margin: 10% auto;
+  flex-direction: column;
+  padding: 10px;
+}
+
+</style>
