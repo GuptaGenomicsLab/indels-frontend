1d3e33d1140c3b00e87a2ea1638eab059434b6bd
David Kanter Eivin <kanter.dev@gmail.com>
2021-10-26 13:56:47 -0400

    fix: remove 403 error toast for unauthenticated /user calls

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -3,6 +3,7 @@ import { NuxtAxiosInstance } from '@nuxtjs/axios'
 
 export default function({ $axios }: { $axios: NuxtAxiosInstance }) {
   $axios.onError((error) => {
+    if (error.request.responseURL.endsWith('/user') && error.response?.status === 403) return;
     ToastProgrammatic.open({
       message: error.response?.data.message ?? error.response?.statusText ?? 'Failed Request to the Server.',
       type: 'is-danger'
